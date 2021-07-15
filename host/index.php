4028c8cfa257144844608c4804a740cdc36f905b
Theieyrre <crizfmtfm@gmail.com>
2021-07-15 07:56:48 +0300

    Added host index page

--- host/index.php
+++ host/index.php
@@ -6,7 +6,8 @@
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-+0n0xVW2eSR5OomGNYDnhzAbDsOXxcvSN1TPprVMTNDbiYZCxYbOOl7+AMvyTG2x" crossorigin="anonymous">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/fontawesome.min.css" integrity="sha512-OdEXQYCOldjqUEsuMKsZRj93Ht23QRlhIb8E/X0sbwZhme8eUw6g8q7AdxGJKakcBbv7+/PX0Gc2btf7Ru8cZA==" crossorigin="anonymous" referrerpolicy="no-referrer" />
     <link rel="shortcut icon" href="images/Anfitrion-main.png" type="image/png">
-    <link rel="stylesheet" href="/styles/main.css">
+    <link rel="stylesheet" href="../styles/main.css">
+    <link rel="stylesheet" href="../styles/host_index.css">
     <title>Anfitrión - Online Restoran Rezervasyon</title>
 </head>
 <body>
@@ -17,9 +18,25 @@
     //    header("Location: login.php");
     //}
 ?>
+<div class="container">
+    <h1 class="welcome">%Restoran adı% için rezervasyonlar</h1>
+    <div class="row">
+        <div class="col">
+            <?php 
+            // Iterative olarak tarih için ASC olarak sırala ve yazdır
+            ?>
+            <div class="rezervasyon">
+                <span>%saat%</span>
+                <?php 
+                // Iterative olarak menüleri listele
+                ?>
+                <p class="menu">%menu adı% - <span>%sayı% adet</span></p>
+            </div>
+        </div>
+    </div>
+</div>
 <footer>
-Frame vector created by pch.vector - www.freepik.com
-Background photo created by wirestock - www.freepik.com
+<a href='https://www.freepik.com/photos/party'>Party photo created by KamranAydinov - www.freepik.com</a>
 </footer>
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.1/dist/js/bootstrap.bundle.min.js" integrity="sha384-gtEjrD/SeCtmISkJkNUaaKMoLD0//ElJ19smozuHV6z3Iehds+3Ulb9Bn9Plx0x4" crossorigin="anonymous"></script>    
 </body>
