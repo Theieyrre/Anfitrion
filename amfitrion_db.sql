98ff226c5d6595d3dfa955d1a7630b2a09d092d7
Mustafa <43235845+Theieyrre@users.noreply.github.com>
2021-07-21 09:49:00 +0300

    New sql output

---
+++ amfitrion_db.sql
@@ -0,0 +1,44 @@
+-- --------------------------------------------------------
+-- Sunucu:                       eu-cdbr-west-01.cleardb.com
+-- Sunucu sürümü:                5.6.50-log - MySQL Community Server (GPL)
+-- Sunucu İşletim Sistemi:       Linux
+-- HeidiSQL Sürüm:               11.3.0.6295
+-- --------------------------------------------------------
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET NAMES utf8 */;
+/*!50503 SET NAMES utf8mb4 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+-- Veri çıktısı seçilmemişti
+
+-- Veri çıktısı seçilmemişti
+
+-- Veri çıktısı seçilmemişti
+
+-- Veri çıktısı seçilmemişti
+
+-- Veri çıktısı seçilmemişti
+
+-- Veri çıktısı seçilmemişti
+
+-- Veri çıktısı seçilmemişti
+
+-- Veri çıktısı seçilmemişti
+
+-- Veri çıktısı seçilmemişti
+
+-- Veri çıktısı seçilmemişti
+
+-- Veri çıktısı seçilmemişti
+
+-- Veri çıktısı seçilmemişti
+
+-- Veri çıktısı seçilmemişti
+
+/*!40101 SET SQL_MODE=IFNULL(@OLD_SQL_MODE, '') */;
+/*!40014 SET FOREIGN_KEY_CHECKS=IFNULL(@OLD_FOREIGN_KEY_CHECKS, 1) */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40111 SET SQL_NOTES=IFNULL(@OLD_SQL_NOTES, 1) */;
