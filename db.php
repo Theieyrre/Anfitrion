f86cbee62792d2e8b7c76a26f97eaa7b97c03ea7
oguzocaletu <72374895+oguzocaletu@users.noreply.github.com>
2021-07-23 23:53:02 +0300

    Update db.php

--- db.php
+++ db.php
@@ -1,6 +1,8 @@
 <?php
-    require_once 'connect.php';
+    require_once 'connect_local.php';
     require_once 'insert.php';
     require_once 'search.php';
     require_once 'checks.php';
+    require_once 'insertWithJoins.php';
+    require_once 'sets.php';
 ?>
