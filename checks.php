e2d3ffc5d4fa490eeacc2462f1f811a3e731c7eb
oguzocaletu <72374895+oguzocaletu@users.noreply.github.com>
2021-07-27 10:43:17 +0300

    Update checks.php

--- checks.php
+++ checks.php
@@ -17,6 +17,40 @@
         echo "Girilen Sifre Yanlıstir!\n";
         return 0;
     }
+    function checkHost($mail,$User_password){
+        include 'connect.php';
+        
+        $id=GetUserID("$mail");
+     
+        if(!$id){
+            return 0;
+        }
+        $is_host=GetTableInfoWithAnyKey("host","user_id",$id);
+        if($is_host!=0&&$User_password==GetUserInfo($id)['password']){
+            echo "Giris Basarili !";
+            return $is_host;
+        }
+
+        echo "Girilen Sifre Yanlıstir!\n";
+        return 0;
+    }
+    function checkClient($mail,$User_password){
+        include 'connect.php';
+        
+        $id=GetUserID("$mail");
+        
+        if(!$id){
+            return 0;
+        }
+        $is_host=GetTableInfoWithAnyKey("client","user_id",$id);
+        if($is_host!=0&&$User_password==GetUserInfo($id)['password']){
+            echo "Giris Basarili !";
+            return 1;
+        }
+
+        echo "Girilen Sifre Yanlıstir!\n";
+        return 0;
+    }
 
     //echo (checkUser("user@mail",123));
 ?>
