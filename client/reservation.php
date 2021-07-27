91bd76b8201a03324b819b2ea4cb50760e513429
oguzocaletu <72374895+oguzocaletu@users.noreply.github.com>
2021-07-27 16:58:26 +0300

    Update reservation.php

--- client/reservation.php
+++ client/reservation.php
@@ -4,122 +4,122 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-+0n0xVW2eSR5OomGNYDnhzAbDsOXxcvSN1TPprVMTNDbiYZCxYbOOl7+AMvyTG2x" crossorigin="anonymous">
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/fontawesome.min.css" integrity="sha512-OdEXQYCOldjqUEsuMKsZRj93Ht23QRlhIb8E/X0sbwZhme8eUw6g8q7AdxGJKakcBbv7+/PX0Gc2btf7Ru8cZA==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css" integrity="sha512-iBBXm8fW90+nuLcSKlbmrPcLa0OT92xO1BIsZ+ywDWZCvqsWgccV3gFoRBv0z+8dLJgyAHIhR35VZc2oM/gI1w==" crossorigin="anonymous" referrerpolicy="no-referrer"/>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-datepicker/1.8.0/css/bootstrap-datepicker.min.css" />
     <link rel="shortcut icon" href="../images/Anfitrion-main.png" type="image/png">
+    <link rel="stylesheet" type="text/css" href="https://code.jquery.com/ui/1.12.0/themes/smoothness/jquery-ui.css">
     <link rel="stylesheet" href="../styles/main.css">
-    <link rel="stylesheet" href="../styles/form.css">
-    <link rel="stylesheet" href="../styles/reservation.css">
+    <link rel="stylesheet" href="../styles/client_index.css">
     <title>Anfitrión - Online Restoran Rezervasyon</title>
 </head>
 <body>
 <?php 
     include '../db.php';
     session_start();
-    //if(!isset($_SESSION["username"])){
-    //    header("Location: /client/login.php");
-    //}
-    if(isset($_GET["id"])){
-        
+    if(!isset($_SESSION["username"])){
+        header("Location: /client/login.php");
     }
-    $restoranBilgileri=GetRestaurantInfo(GetRestaurantID($_POST["name"]));//restoran name ile id bulunabilir
-    if(restoranBilgileri)
-    {
-        $isopen=($restoranBilgileri["close_time"]>$_POST["hour"])&&($restoranBilgileri["open_time"]=<$_POST["hour"]);
-
-    }
-    if(!$isopen){
-        echo "restoran belirtilen saatlerde acik degildir";
-    }
-    
-    
 ?>
+<div class="container">
+    <h1 class="welcome">Hoşgeldiniz, <?php print($_SESSION["username"]) ?></h1>
+    <?php  
+    $reservation=GetTableInfoWithAnyKey("reservation","client_id",$_SESSION["client_id"]))
+    if($reservation==0) {
+         ?>
+    <div class="row">
+        <div class="col-2">
+            <form action="index.php" method="post">
+                <p class="label">Rezervasyon tarihini seçiniz</p>
+                <input type="text" name="date" class="date" data-provide="datepicker">
+                <p class="label">Hangi mutfaktan istersiniz ?</p>
+                <select name="category">
+                    <?php 
+                        // restoranların kategorisi yok 
 
-<div class="row d-flex justify-content-center">
-        <form method="post" class="reservation">
-                <p class="label">Restoran adı</p>
-                <input type="text" name="restaurant" class="name" disabled>
-                <p class="label">Tarih</p>
-                <input type="text" name="date" class="date" disabled>
-                <p class="label">Kaç kişi olacaksınız ?</p>
-                <input type="number" id="person" name="person">
-                <p class="label">Ne zaman geleceksiniz ?</p>
-                <input type="time" name="hour" min="%restoran_min" max="%restoran_max" required>
-                <div id="menus" class="carousel slide" data-ride="carousel">
-                <ol class="carousel-indicators">
-                    <?php // Oluşturduğun menü kadar li de üret?>
-                    <li data-target="#menus" data-slide-to="0" class="active"></li>
-                    <li data-target="#menus" data-slide-to="1"></li>
-                </ol>
-                <div class="carousel-inner">
-                    <?php // Gerektiği kadar menüyü iterative aşağı kod sekmesini kullanarak oluştur. daily değilse classta daily olmasın?>
-                        <div class="menu-card daily carousel-item active">
-                            <div class="menu-card-header">
-                                Günün Menüsü 
-                            </div>
-                            <div class="menu-card-body">
-                                <?php // Gerektiği kadar kategori ile iterative aşağı kod sekmesini kullanarak menu oluştur?>
-                                <h5 class="menu-card-title">%foodcategori%</h5>
-                                <p class="menu-card-text">%foodaçıklama%</p>
-                                <p class="label">Kaç kişi bu menüden alıcak ?</p>
-                                <input type="number" class="max_person" min="0" name="amount">
-                            </div>
-                        </div>
-                    <?php // Gerektiği kadar card div içerisini iterasyon ile üret ve doldur?>
-                    <div class="menu-card carousel-item">
-                            <div class="menu-card-header">
-                                Menü 1 
-                            </div>
-                            <div class="menu-card-body">
-                                <?php // Gerektiği kadar kategori ile iterative aşağı kod sekmesini kullanarak menu oluştur?>
-                                <h5 class="menu-card-title">%foodcategori%</h5>
-                                <p class="menu-card-text">%foodaçıklama%</p>
-                                <p class="label">Kaç kişi bu menüden alıcak ?</p>
-                                <input type="number" class="max_person" min="0" name="amount">
-                            </div>
-                    </div>
+                        //GetAllRestaurantInfo();// KULLANARAK TÜM RESTORAN BİLGİLERİNİ FOREACH İLE ALABİLİRSİN
+                        //GetRestaurantInfo($RestaurantID);// BUNUNLA İSE İD İLE RESTORAN ÇEKEBİLİRSİN 
+                        //GetRestaurantInfo(GetRestaurantID($Name));// BUNUNLA İSE RESTORAN NAME İLE RESTORAN ÇEKEBİLİRSİN 
+
+                        // Restoran kategorilerini çekip burda option içerisine listele
+                    ?>
+                    <option value="%kategori_adı%">%kategori adı%</option>
+                </select>
+                <p class="label">Restoran minimum yıldız değeri seçiniz</p>
+                <div class="stars">
+                    <input type="radio" name="rating" id="rating1">
+                    <label for="rating1" class="fas fa-star"></label>
+                    <input type="radio" name="rating" id="rating2">
+                    <label for="rating2" class="fas fa-star"></label>
+                    <input type="radio" name="rating" id="rating3">
+                    <label for="rating3" class="fas fa-star"></label>
+                    <input type="radio" name="rating" id="rating4">
+                    <label for="rating4" class="fas fa-star"></label>
+                    <input type="radio" name="rating" id="rating5">
+                    <label for="rating5" class="fas fa-star"></label>
                 </div>
-                <a class="carousel-control-prev" href="#menus" role="button" data-slide="prev">
-                    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-                    <span class="sr-only">Previous</span>
+                <button type="submit" class="btn btn-secondary" value="uygula" name="apply">Uygula</button> 
+            </form>
+        </div>
+        <div class="col">
+            <div class="restaurant">
+                <span>%restoran adı%</span>
+                <span>%mevcut rezervasyon%</span>
+                <a href="reservation.php?id=%restaurant_id%">
+                    <button type="button" class="btn btn-success">
+                        Rezerve Et
+                    </button>
                 </a>
-                <a class="carousel-control-next" href="#menus" role="button" data-slide="next">
-                    <span class="carousel-control-next-icon" aria-hidden="true"></span>
-                    <span class="sr-only">Next</span>
-                </a>
-                </div>
-        </form>
+            </div>
+        </div>
+    </div>
+    <?php }else{ ?>
+        <div class="row">
+        <div class="col-6">
+            <h1 class="title">
+                Mevcut rezervasyonunuz
+            </h1>
+            <div class="restaurant">
+                <form method="post">
+                    <span><?php echo GetRestaurantInfo($reservation["restaurant_id"])["name"]?></span>
+                    
+                    <span><?php echo $reservation["date"]?></span>
+                    <span><?php echo $reservation["person_count"]?></span>
+                    <button type="button" class="btn btn-danger">İPTAL ET</button>
+                </form>
+            </div>
+        </div>
+    </div>    
+    <?php } ?>
 </div>
 
-<?php
-    if(isset($_POST["login"])){
-        $has_participated=0;//daha katılmadı
-        CreateReservation (GetRestaurantID($_POST["name"]),
-         $userid,$_POST["number"],$_POST["note"],$has_participated);
-
-        // Giriş yapma işlemleri bura yazılacak  // bunlar zaten üye olanlara gözükmüyor mu?
-        // Olmayan üye için registera yönlendir   // başta login ederse anca bu ekran gelsin bence
-        // Doğru giriş yapılırsa /client/ 'a yönledir
+<?php 
+    //if(!isset($_SESSION["username"])){
+    //    header("Location: login.php");
+    //}
+    if (isset($_POST['button'])){
+        if($reservation!=0){
+            Delete("reservation","client_id",$_SESSION["client_id"]);
+        }
+        else{
+            header("Location: /client/reservation.php");
+         }
+    }
+    if(isset($_POST["apply"])){
+        // Filtre değerlerini burda okuyup uygun şekilde sayfayı reload et
+        // Star rating için en son set edilen değer star değerini verir rating5 ise 5 yıldız seçmiştir mesela
     }
 ?>
 <footer>
-<a href='https://www.freepik.com/photos/food'>Food photo created by betochagas - www.freepik.com</a>
+<a href='https://www.freepik.com/photos/food'>Food photo created by KamranAydinov - www.freepik.com</a>
 </footer>
 <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
 <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
 <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
 <script src="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-datepicker/1.8.0/js/bootstrap-datepicker.min.js"></script>
-<script type="text/javascript">
+<script>
     $(document).ready(function() {
         $(".date").datepicker({dateFormat: "dd-mm-yyyy"});
         $(".date").datepicker("setDate", new Date());
-
-        var person_count = 0;
-        $("#person").change(function(){
-            person_count = $(this).val();
-            $(".max_person").each(function(){
-                $(this).attr("max", person_count);
-            });
-        });
     });
 </script>
 </body>
