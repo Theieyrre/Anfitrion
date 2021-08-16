b34cbb1d1f184e292e476fd65ef4ccaca8a5bb17
oguzocaletu <72374895+oguzocaletu@users.noreply.github.com>
2021-08-16 18:44:46 +0300

    Update register.php

--- host/register.php
+++ host/register.php
@@ -41,7 +41,8 @@
 <?php
     if(isset($_POST["signup"])){
         if(GetUserID($_POST["email"])==0){
-            $userid=CreateUser ($_POST["email"],$_POST["phonenumber"],$_POST["password"],$_POST["firstname"],$_POST["lastname"] );
+            $postbid=CreatePostBox("");
+            $userid=CreateUser ($postbid,$_POST["email"],$_POST["phonenumber"],$_POST["password"],$_POST["firstname"],$_POST["lastname"] );
             $_SESSION["user_id"]=$userid;
             $_SESSION["username"]=$_POST["email"];
             $host_id= CreateHost($userid,0,"");
