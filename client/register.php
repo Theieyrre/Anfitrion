39d5c97e0414aed9f20393bbd3669eea38e35b92
oguzocaletu <72374895+oguzocaletu@users.noreply.github.com>
2021-08-16 18:47:20 +0300

    Update register.php

--- client/register.php
+++ client/register.php
@@ -41,7 +41,8 @@
 <?php
     if(isset($_POST["signup"])){
         if(GetUserID($_POST["email"])==0){
-            $client_id=CreateClient ($_POST["email"],$_POST["phonenumber"],$_POST["password"],$_POST["firstname"],$_POST["lastname"] );
+            $postbid=CreatePostBox("");
+            $client_id=CreateClient ($postbid,$_POST["email"],$_POST["phonenumber"],$_POST["password"],$_POST["firstname"],$_POST["lastname"] );
             $_SESSION["client_id"]=$client_id;
             $_SESSION["username"]=$_POST["email"];
             $_SESSION["firstname"]=$_POST["firstname"];
