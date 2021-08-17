1f743925ba6351ea5572bbac634467701caa94b2
Theieyrre <crizftfm@gmail.com>
2021-08-17 09:57:47 +0300

    Update landing page

--- index.php
+++ index.php
@@ -19,10 +19,37 @@
     }
 ?>
     <div class="container">
-        <a class="navbar-brand" href="index.php">
-            <img src="images/Anfitrion-hover.png" alt="" height="100px">
-            Anfitrión
-        </a>
+        <div class="row">
+            <a class="navbar-brand" href="index.php">
+                <img src="images/Anfitrion-hover.png" alt="" height="100px">
+                Anfitrión
+            </a>
+        </div>
+        <div class="row calltoaction">
+            <h1>Anfitrion nedir?</h1>
+            <p>Anfitrion, rezervasyon yapmanızı kolaylaştıran bir uygulamadır. Alışılmış telefonla rezervasyon yerine daha yenilikçi ve kullanımı kolay bir uygulamadır.</p>          
+            <div class="lists">
+                <div class="list">
+                    <p class="list-header">Müşteriler için</p>
+                    <ul>
+                        <li>Rezervasyon öncesi restorandaki menüleri ve fiyatları görebilmenizi sağlar</li>
+                        <li>Siparişinizi önceden verebiliriz ve geldiğinizde hazır olmasını sağlayabilirsiniz</li>
+                        <li>Önceki müşterilerin yorumlarını ve restoranın toplam değerlendirmesini görüntüleyebilirsiniz</li>
+                        <li>En iyi restoranları ve farklı mutfakları kolayca filtreleyip seçme imkanı sağlar</li>
+                    </ul>
+                </div>
+                <div class="list">
+                    <p class="list-header">Eğer restoran sahibiyseniz</p>
+                    <ul>
+                        <li>Rezevasyonlarınızı bir arada gösterir ve siparişlerinizi kolayca görüntüleybilirsiniz</li>
+                        <li>Kolayca yiyeceklerinizi ve menülerinizi tanımlayabilirsiniz ve gerektiğinde güncelleme yapabilirsiniz</li>
+                        <li>Müşterilerin yorumlarına cevap verebilir</li>
+                        <li>Restoranınız için reklam imkanı ve iş olanağı sağlar</li>
+                    </ul>
+                </div>
+                
+            </div> 
+        </div>
     </div>
     <div class="buttons d-flex justify-content-center">
         <a href="client">
