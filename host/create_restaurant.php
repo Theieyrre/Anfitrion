f01be5c179497e9ce467922e780222648ac554af
oguzocaletu <72374895+oguzocaletu@users.noreply.github.com>
2021-08-17 17:26:34 +0300

    Update create_restaurant.php

--- host/create_restaurant.php
+++ host/create_restaurant.php
@@ -54,7 +54,7 @@
     if(isset($_POST["save"])){
         $restaurant_id=CreateRestourant ($_POST["name"],$_POST["desc"],$_POST["masa_sayisi"],$_POST["MasaCapacity"],$_POST["days[]"],$_POST["open_time"],$_POST["close_time"]);
         $_SESSION["restaurant_id"]=$restaurant_id;
-        Set("host",$_SESSION["host_id"],"restaurant_id",$restaurantid);
+        Set("host",$_SESSION["host_id"],"restaurant_id",$restaurant_id);
         Set("host",$_SESSION["host_id"],"restaurant_name",$_POST["name"]);
         header("Location: add_food.php");
     }
